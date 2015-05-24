4dccfa32988173d0c445d5d4cbfffbd77095f045
ryanicle <ryanicle@gmail.com>
2015-05-24 03:47:12 +0000

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name creditCardApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the creditCardApp
+ */
+angular.module('creditCardApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
